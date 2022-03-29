a1a0ee7f652ea9d1f072bce1c393269e909e050c
Nika Kotliar <wanderer2805@gmail.com>
2022-03-29 09:33:38 +0300

    Database 2.1 done

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.CharField(max_length=200)
+    release_date = models.DateTimeField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f'{self.name}, {self.price}, {self.image}, {self.release_date}, {self.lte_exists}, {self.slug}'
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.shortcuts import render, redirect
+
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +9,44 @@ def index(request):
 
 
 def show_catalog(request):
+    sort_by = request.GET.get('sort')
+    print(sort_by)
     template = 'catalog.html'
-    context = {}
+    if sort_by == 'name':
+        phone_objects = Phone.objects.order_by('name').all()
+    elif sort_by == 'min_price':
+        phone_objects = Phone.objects.order_by('price').all()
+    elif sort_by == 'max_price':
+        phone_objects = Phone.objects.order_by('price').all().reverse()
+
+    phones = [{
+        'slug': phone.slug,
+        'name': phone.name,
+        'price': phone.price,
+        'image': phone.image
+    } for phone in phone_objects]
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone_objects = Phone.objects.filter(slug=slug)
+    if len(phone_objects) > 0:
+        phone_result = phone_objects[0]
+        print(phone_result)
+        phone = {
+            'name': phone_result.name,
+            'price': phone_result.price,
+            'image': phone_result.image,
+            'release_date': phone_result.release_date,
+            'lte_exists': phone_result.lte_exists
+        }
+        context = {
+            'phone': phone
+        }
+        return render(request, template, context)
+    else:
+        return HttpResponseNotFound(f'Такой модели нет')
