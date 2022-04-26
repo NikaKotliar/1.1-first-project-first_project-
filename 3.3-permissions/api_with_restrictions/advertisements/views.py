fa706589e5b2f3d8a68e057bb11ff0049b78e563
Nika Kotliar <wanderer2805@gmail.com>
2022-04-26 16:41:49 +0300

    3.3 corrected

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,22 +1,16 @@
 from django_filters import FilterSet, DateFromToRangeFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.filters import SearchFilter
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
-from api_with_restrictions.permissions import IsAutoriseOrReadOnly, IsOwnerOrReadOnly
+from advertisements.permissions import IsAutoriseOrReadOnly, IsOwnerOrReadOnly
 
 
-class F(FilterSet):
-    created_at = DateFromToRangeFilter()
 
-    class Meta:
-        model = Advertisement
-        fields = ['created_at', 'creator', 'status']
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -32,9 +26,9 @@ class AdvertisementViewSet(ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAutoriseOrReadOnly, IsOwnerOrReadOnly)
-    filter_backends = [DjangoFilterBackend]
-    filter_class = F
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    filter_backends = [DjangoFilterBackend]
+    filter_class = AdvertisementFilter
 
     # def get_permissions(self):
     #     """Получение прав для действий."""
